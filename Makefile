2bf1ec1c4bbced3d62e9a9a58a4de17107b5ac8d
Jae <jeonjaeyeong@jeonjaeyeong-ui-MacBookAir.local>
2021-11-21 18:01:12 +0900

    21.11.21/17.2

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: jaeyjeon <@student.42seoul.kr>             +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/11/20 19:29:07 by jaeyjeon          #+#    #+#              #
-#    Updated: 2021/11/21 17:36:36 by jaeyjeon         ###   ########.fr        #
+#    Updated: 2021/11/21 18:00:52 by jaeyjeon         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -18,8 +18,7 @@ CFLAGS		= -Wall -Wextra -Werror
 
 SRCS		= ft_memset.c ft_memmove.c ft_memcpy.c ft_isprint.c ft_isdigit.c \
 				ft_isascii.c ft_isalpha.c ft_isalnum.c ft_bzero.c ft_strlcat.c \
-				ft_strlcpy.c 
-OBJS		= $(SRCS:.c=.o)
+				ft_strlcpy.c ft_tolower.c ft_toupper.c
 
 all:		$(NAME)
 
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: jaeyjeon <@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/19 14:21:36 by jaeyjeon          #+#    #+#             */
-/*   Updated: 2021/11/20 19:45:29 by jaeyjeon         ###   ########.fr       */
+/*   Updated: 2021/11/21 18:00:55 by jaeyjeon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,9 +22,12 @@ int     ft_isdigit(int c);
 int     ft_isalnum(int c);
 int     ft_isascii(int c);
 int     ft_isprint(int c);
+int     ft_toupper(int c);
+int     ft_tolower(int c);
 
 size_t  ft_strlen(const char *s);
 size_t  ft_strlcpy(char *dst, const char *src, size_t size);
+size_t  ft_strlcat(char *dst, const char *src, size_t size);
 
 void    *ft_memset(void *b, int c, size_t len);
 void    ft_bzero(void *s, size_t n);
