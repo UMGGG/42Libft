5705661cafead113b24b2cd44de76911f57a04ee
Jae <jeonjaeyeong@jeonjaeyeong-ui-MacBookAir.local>
2021-12-10 02:04:21 +0900

    21.12.10/02

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,33 +6,35 @@
 /*   By: jaeyjeon <@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/21 17:21:25 by jaeyjeon          #+#    #+#             */
-/*   Updated: 2021/11/29 17:58:48 by jaeyjeon         ###   ########.fr       */
+/*   Updated: 2021/12/10 02:03:25 by jaeyjeon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlen(const char *s)
-{
-	size_t	len;
-
-	len = 0;
-	while (s[len])
-	{
-		len++;
-	}
-	return (len);
-}
-
 size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
 	size_t	left;
+	size_t	dstlen;
+	size_t	srclen;
 
 	left = size;
-	if (size < strlen(dst))
-		return ((size_t)(size + strlen(src)));
-	while (*dst == '\0' || left != 0)
+	dstlen = ft_strlen(dst);
+	srclen = ft_strlen(src);
+	if (size <= ft_strlen(dst))
+		return (size + srclen);
+	while (*dst != '\0' && left != 0)
 	{
-
+		dst++;
+		left--;
 	}
+	while (left != 1 && *src != '\0')
+	{
+		*dst = *src;
+		dst++;
+		src++;
+		left--;
+	}
+	*dst = '\0';
+	return (srclen + dstlen);
 }
